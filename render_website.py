842fc91cecd21a6c46e332a3033192093aa950ab
gennadynemchin@gmail.com <gennadynemchin@gmail.com>
2023-06-01 23:24:03 +0300

    Add iteration over books;

---
+++ render_website.py
@@ -0,0 +1,34 @@
+import os
+import json
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+from livereload import Server, shell
+from dotenv import load_dotenv
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+from more_itertools import chunked
+
+
+with open('json/books_info.json', 'r') as file:
+    books_info = json.load(file)
+
+def main():
+    load_dotenv()
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
+
+    template = env.get_template('template.html')
+    rendered_page = template.render(
+            books_info=books_info['books_features']
+    )
+
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
+
+    server = Server()
+    server.watch('*.html', main)
+    server.serve(root='.')
+
+
+if __name__ == '__main__':
+    main()
